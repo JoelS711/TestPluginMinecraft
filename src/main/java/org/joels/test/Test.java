eed94d11df52cf75acae5efcd7da7209f4df7f0b
Emanuell Gelvez <joelsanabria11@gmail.com>
2025-02-11 09:52:55 -0500

    Update Test.java

--- src/main/java/org/joels/test/Test.java
+++ src/main/java/org/joels/test/Test.java
@@ -1,17 +1,29 @@
 package org.joels.test;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Test extends JavaPlugin {
+public final class Test extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e){
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.RED + "Stop Moving! You are Frozen");
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e){
+        e.getPlayer().sendMessage("You just threw an egg!");
     }
 }
