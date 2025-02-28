7030dad47ab28ccdd755e526432cbda1e31ab15f
Emanuell Gelvez <joelsanabria11@gmail.com>
2025-02-28 09:52:33 -0500

    Update Test.java

--- src/main/java/org/joels/test/Test.java
+++ src/main/java/org/joels/test/Test.java
@@ -1,29 +1,29 @@
 package org.joels.test;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerEggThrowEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Test extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
-
+        getCommand("heal").setExecutor(new HealCommand());
     }
 
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent e){
-        e.setCancelled(true);
-        e.getPlayer().sendMessage(ChatColor.RED + "Stop Moving! You are Frozen");
-    }
-
-    @EventHandler
-    public void onPlayerEggThrow(PlayerEggThrowEvent e){
-        e.getPlayer().sendMessage("You just threw an egg!");
-    }
+//    @Override
+//    public void onEnable() {
+//        Bukkit.getPluginManager().registerEvents(this, this);
+//
+//    }
+//
+//    @EventHandler
+//    public void onPlayerMove(PlayerMoveEvent e){
+//        e.setCancelled(true);
+//        e.getPlayer().sendMessage(ChatColor.RED + "Stop Moving! You are Frozen");
+//    }
+//
+//    @EventHandler
+//    public void onPlayerEggThrow(PlayerEggThrowEvent e){
+//        e.getPlayer().sendMessage("You just threw an egg!");
+//    }
 }
